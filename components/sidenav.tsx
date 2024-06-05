e8ff4ba5b3ed9962f99f969026042653a5959f66
Rafael Cabrera <raffeyrera@gmail.com>
2024-06-05 18:14:21 +0800

    fixed vercel deploy error from sidenav

--- components/sidenav.tsx
+++ components/sidenav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './SideNav.module.css';
+import styles from './sideNav.module.css';
 
 const SideNav: React.FC = () => {
   return (
